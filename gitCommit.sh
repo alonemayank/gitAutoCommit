4fe14e5effe1a29b29abdef4c119d98645317ec7
Apurva Tripathi <alonemayank@gmail.com>
2017-11-17 21:11:22 +0000

    Updating log file

--- gitCommit.sh
+++ gitCommit.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+NUMBER_N=$[ ($RANDOM%10) +1 ]
+echo "Random Number $NUMBER_N"$'\n'
+I=0
+while [ $I -le $NUMBER_N ]
+do
 
-date > ~/nodeProject/date.txt
+	date > ~/nodeProject/date.txt
 
-git -C ~/nodeProject/ add .
-git -C ~/nodeProject/ commit -m "Updating log file"
-git -C ~/nodeProject/ push
+	git -C ~/nodeProject/ add .
+	git -C ~/nodeProject/ commit -m "Updating log file"
+	git -C ~/nodeProject/ push
+	sleep $[ ( $RANDOM % 20 )  + 1 ]m
+done
